9fcf0ab0c87a8837774e31c3332c831d1dfbcb97
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-26 20:41:27 +0000

    Snake Game

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const { connect } = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const hendleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+  hendleUserInput();
+  return stdin;
+};
+
 console.log('Connecting ...');
 connect();
