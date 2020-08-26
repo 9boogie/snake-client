5faa247e4bbaddd299c5e6c72a8e08009d998438
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-26 20:29:12 +0000

    Snake Game

--- client.js
+++ client.js
@@ -5,8 +5,19 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
-  // interpret incoming data as text
+
   conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: JAE');
+  })
+  
+  conn.on('connect', () => {
+    setInterval(() => {
+      conn.write("Move: left")
+    }, 100);
+  })
   
   conn.on('data', (data) => {
     console.log(`server says: ${data}`)
