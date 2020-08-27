a502be3e5baadf714a269d74d68eb2c09bfcac0d
JaeyoungKim <jzero.youngs89@gmail.com>
2020-08-27 00:28:31 +0000

    Snake Game

--- input.js
+++ input.js
@@ -15,18 +15,15 @@ const handleUserInput = function(key) {
     process.exit();
   } else if (key === 'w') {
     connection.write('Move: up');
-    console.log('up');
   } else if (key === 'a') {
     connection.write('Move: left');
-    console.log('left')
   } else if (key === 's') {
     connection.write('Move: down');
-    console.log('down')
   } else if (key === 'd') {
     connection.write('Move: right');
-    console.log('right')
-  } else {
-    console.log('waiting')
+  } else if (key === 'k') {
+    let word = key;
+    connection.write('Say: hey');
   }
 };
 
